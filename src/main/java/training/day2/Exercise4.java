2977f8e0ae1b410d4ea301bec0d998da9377e235
123Boryana <glavchevaboryana@gmail.com>
2023-07-04 11:55:44 +0300

    Initial commit Exercise 4

---
+++ src/main/java/training/day2/Exercise4.java
@@ -0,0 +1,10 @@
+package training.day2;
+
+public class Exercise4 {
+    public static void main(String[] args){
+        String[] daysOfWeek = {"Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"};
+
+        for(String day : daysOfWeek)
+            System.out.println(day);
+    }
+}
