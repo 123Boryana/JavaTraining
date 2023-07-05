76373f1a89093b09e3e9e0772484eb5fd74b5a85
123Boryana <glavchevaboryana@gmail.com>
2023-07-05 14:25:46 +0300

    Initial commit updated exercises

--- src/main/java/training/day3/Exercise5.java
+++ src/main/java/training/day3/Exercise5.java
@@ -4,22 +4,26 @@ import java.util.Collections;
 import java.util.Scanner;
 public class Exercise5 {
 
-    public static void swapper(ArrayList<String> list){
-        if(list.size() >= 2){
-            Collections.swap(list, 0, list.size() - 1 );
+    public static void swap(ArrayList<String> list) {
+        if (list.size() >= 2) {
+            Collections.swap(list, 0, list.size() - 1);
         }
     }
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> list = new ArrayList<>();
-        System.out.println("Enter the elements of your list: ");
-        String input = scanner.next();
 
-        while (!input.isEmpty()){
+        System.out.println("Enter the elements of the ArrayList: ");
+        String input = scanner.nextLine();
+        while (!input.isEmpty()) {
             list.add(input);
-            input = scanner.next();
+            input = scanner.nextLine();
         }
 
-        swapper(list);
+        swap(list);
+
+        System.out.println("After swap: " + list);
+
+        scanner.close();
     }
 }
