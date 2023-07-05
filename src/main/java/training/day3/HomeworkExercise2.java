7d72ff3022850eda8f316b46a63fda2379846c45
123Boryana <glavchevaboryana@gmail.com>
2023-07-05 20:35:52 +0300

    Initial commit homework exercises 1 and 2

---
+++ src/main/java/training/day3/HomeworkExercise2.java
@@ -0,0 +1,18 @@
+package training.day3;
+
+import java.util.Scanner;
+public class HomeworkExercise2 {
+
+    public static void evenOrOdd(int i){
+        if(i % 2 == 0)
+            System.out.println("true");
+        else
+            System.out.println("false");
+    }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write your number: ");
+        int number = scanner.nextInt();
+        evenOrOdd(number);
+    }
+}
