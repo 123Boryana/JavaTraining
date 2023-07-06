45ced05de9621abdea899b4a661defbcc4cef40c
123Boryana <glavchevaboryana@gmail.com>
2023-07-06 11:49:28 +0300

    Initial commit Updated homework exercises

--- src/main/java/training/day3/HomeworkExercise2.java
+++ src/main/java/training/day3/HomeworkExercise2.java
@@ -3,13 +3,14 @@ package training.day3;
 import java.util.Scanner;
 public class HomeworkExercise2 {
 
-    public static void evenOrOdd(int i){
-        if(i % 2 == 0)
+    public static void evenOrOdd(int i) {
+        if (i % 2 == 0)
             System.out.println("true");
         else
             System.out.println("false");
     }
-    public static void main(String[] args){
+    
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Write your number: ");
         int number = scanner.nextInt();
