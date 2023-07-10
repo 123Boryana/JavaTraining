311e9df96736a98ecbcae4402152130a2a27b9e7
123Boryana <glavchevaboryana@gmail.com>
2023-07-10 09:21:08 +0300

    Initial commit Updated homework exercise

--- src/main/java/training/day4/Car.java
+++ src/main/java/training/day4/Car.java
@@ -1,7 +1,7 @@
 package training.day4;
 
 public class Car extends Vehicle {
-    final static double DEFAULT_FUEL_CONSUMPTION = 3;
+    private final static double DEFAULT_FUEL_CONSUMPTION = 3;
     public Car(double fuel, int horsePower) {
         super(fuel, horsePower);
     }
