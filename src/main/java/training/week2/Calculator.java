849c96798d1ae541a4641a4234847c1919f6881d
123Boryana <glavchevaboryana@gmail.com>
2023-07-11 10:37:08 +0300

    Initial commit updated exercise

--- src/main/java/training/week2/Calculator.java
+++ src/main/java/training/week2/Calculator.java
@@ -10,25 +10,29 @@ public class Calculator {
         System.out.println("Write your second number: ");
         double num2 = scanner.nextDouble();
 
-        System.out.println("Enter the operator (+, -, *, /, addition, subtraction, multiplication, division)");
+        System.out.println("Enter the operator (+, -, *, /, addition, subtraction, multiplication, division, plus, minus, multiplied by, divided by)");
         String operator = scanner.next();
         double value = 0;
 
         switch (operator) {
             case "+":
             case "addition":
+            case "plus":
                 value = num1 + num2;
                 break;
             case "-":
             case "subtraction":
+            case "minus":
                 value = num1 - num2;
                 break;
             case "*":
             case "multiplication":
+            case "multiplied by":
                 value = num1 * num2;
                 break;
             case "/":
             case "division":
+            case "divided by":
                 value = num1 / num2;
             default:
                 System.out.println("Wrong entry");
